357f00250668202b0f07a4e3ec550205926584fe
zLouis043 <louis.003.lt@gmail.com>
2023-08-10 20:21:10 +0200

    fixed errors

--- main.c
+++ main.c
@@ -5,41 +5,45 @@
 
 int main(int argc, char *argv[]){
 
-    zstring str = newZString("Hello, world!");
+    zstring str = newZString("Hello, World! New String Library 2019");
     zstring trimmedString = newZString("");
     zstring removedWordString = newZString("");
     zstring copiedString = newZString("");
     zstring removedCharString = newZString("");
     zstring reversedString = newZString("");
     
-    size_t  occurances = 0;
+    size_t occurances = 0;
+    size_t numOfWords = 0;
     char toFind = 'w';
-    char toRemove[] = "world";
+    char toRemove[] = "World";
     Bool areEqual = False;
 
     str.length = lenOfStr(str.data);
-    printf("String: '%s' of Len: %zu\n", str.data, str.length);
+    printf("String: '%s' of Len: %zu\n\n", str.data, str.length);
 
     trimmedString = trimStr(str);
-    printf("Trimmed: '%s' of len: %zu\n", trimmedString.data, trimmedString.length);
+    printf("Trimmed: '%s' of len: %zu\n\n", trimmedString.data, trimmedString.length);
     
     occurances = findOccuranceOf(str, toFind);
-    printf("In the string: '%s' the '%c' occures %zu times\n", str.data, toFind, occurances);
+    printf("In the string: '%s' the '%c' occures %zu times\n\n", str.data, toFind, occurances);
 
     removedWordString  = removeWord(str, toRemove);
-    printf("Removed '%s' at index %zu to String: '%s' of len: %zu\n", toRemove, findStartOfWord(str, toRemove),removedWordString.data, removedWordString.length);
+    printf("Removed '%s' at index %zu to String: '%s' of len: %zu\n\n", toRemove, findStartOfWord(str, toRemove),removedWordString.data, removedWordString.length);
     
     copiedString = copyStr(str);
-    printf("Copied: '%s' of len: %zu\n", copiedString.data, copiedString.length);
+    printf("Copied: '%s' of len: %zu\n\n", copiedString.data, copiedString.length);
     
     areEqual = compareString(str, toLowercaseStr(str));
-    printf("String 1 : '%s' and String 2: '%s' are equal? %s\n", str.data, toLowercaseStr(str).data, areEqual ? "True" : "False");
+    printf("String 1 : '%s' and String 2: '%s' are equal? %s\n\n", str.data, toLowercaseStr(str).data, areEqual ? "True" : "False");
     
     removedCharString  = removeChar(str, toFind);
-    printf("Removed '%c' to String: '%s' of len: %zu\n", toFind, removedCharString.data, removedCharString.length);
+    printf("Removed '%c' to String: '%s' of len: %zu\n\n", toFind, removedCharString.data, removedCharString.length);
 
     reversedString = reverseStr(str);
-    printf("Reversed: '%s' of len %zu\n", reversedString.data, reversedString.length);
+    printf("Reversed: '%s' of len %zu\n\n", reversedString.data, reversedString.length);
+
+    numOfWords = numberOfWords(str);
+    printf("In this string there are %zu words\n\n", numOfWords);
 
     return 0;
 }
--- src/zstring.h
+++ src/zstring.h
@@ -16,15 +16,18 @@ typedef enum{
 }Bool;
 
 
-size_t  lenOfStr(const char* str);
-size_t  findOccuranceOf(zstring str, char toFind);
-size_t  findStartOfWord(zstring str, char* word);
+size_t lenOfStr(const char* str);
+size_t findOccuranceOf(zstring str, char toFind);
+size_t findStartOfWord(zstring str, char* word);
+size_t numberOfWords(zstring str);
 char toLowerCase(char c);
 char toupperCase(char c);
 Bool isaDigit(char c);
 Bool isaNumber(char c);
 Bool isaSpace(char c);
 Bool isaSymbol(char c);
+Bool isaTerminator(char c);
+Bool isaNewLine(char c);
 Bool isWordInString(zstring str, char* word, int start);
 Bool compareString(zstring str1, zstring str2);
 zstring newZString(const char* str);
@@ -40,20 +43,21 @@ zstring reverseStr(zstring str);
 
 #ifndef ZSTRING_IMPLEMENTATION
 
-size_t  lenOfStr(const char* str){
-    size_t  len = 0;
-    while(str[len] != '\0'){
+size_t lenOfStr(const char* str){
+    size_t len = 0;
+    while(!isaTerminator(str[len])){
         len++;
     }
     return len;
 }
 
-size_t  findOccuranceOf(zstring str, char toFind){
-    size_t  occurences = 0;
-    size_t  i = 0;
-    printf("String to Lower : '%s'\n", toLowercaseStr(str).data);
-    while(toLowercaseStr(str).data[i] != '\0'){
-        if(toLowercaseStr(str).data[i] == toLowerCase(toFind)){
+size_t findOccuranceOf(zstring str, char toFind){
+    size_t occurences = 0;
+    size_t i = 0;
+    zstring toLowCase = toLowercaseStr(str);
+    printf("String to Lower : '%s' | char to find '%c'\n", toLowCase.data, toLowerCase(toFind));
+    while(!isaTerminator(toLowCase.data[i])){
+        if(toLowCase.data[i] == toLowerCase(toFind)){
             occurences++;
         }
         i++;
@@ -61,10 +65,9 @@ size_t  findOccuranceOf(zstring str, char toFind){
     return occurences;
 }
 
-size_t  findStartOfWord(zstring str, char* word){
-    size_t  i = 0; 
-    zstring result = newZString("");
-    while(str.data[i] != '\0'){
+size_t findStartOfWord(zstring str, char* word){
+    size_t i = 0; 
+    while(!isaTerminator(str.data[i])){
         if(str.data[i] == word[0]){
             if(isWordInString(str, word, i)){
                 return i;
@@ -73,6 +76,25 @@ size_t  findStartOfWord(zstring str, char* word){
         i++;
     }
     return False; //if it does not find the word in the string it returns False
+}
+
+size_t numberOfWords(zstring str){
+    size_t i = 0; size_t n = 0;
+
+    if(!isaSpace(str.data[0])){
+        n++;
+    }
+
+    while(!isaTerminator(str.data[i])){
+        while(isaSpace(str.data[i])){
+            i++;
+        }
+        if(!isaSpace(str.data[i]) && isaSpace(str.data[i+1])){
+            n++;
+        }
+        i++;
+    }
+    return n;
 }
 
 char toLowerCase(char c){
@@ -104,9 +126,17 @@ Bool isaSymbol(char c){
     return (!isaDigit(c) && !isaNumber(c) && !isaSpace(c));
 }
 
+Bool isaTerminator(char c){
+    return (c == '\0');
+}
+
+Bool isaNewLine(char c){
+    return (c == '\n');
+}
+
 Bool isWordInString(zstring str, char* word, int start){
-    size_t  i = 0; size_t  j = 1;
-    while(word[j] != '\0'){
+    size_t i = 0; size_t  j = 1;
+    while(!isaTerminator(word[j])){
         if(word[j] != str.data[start+i+1]){
             return False;
         }
@@ -121,8 +151,8 @@ Bool compareString(zstring str1, zstring str2){
         return False;
     }
 
-    size_t  i = 0; 
-    while(str1.data[i] != '\0'){
+    size_t i = 0; 
+    while(!isaTerminator(str1.data[i])){
         if(str1.data[i] != str2.data[i]){
             return False;
         }
@@ -135,34 +165,42 @@ Bool compareString(zstring str1, zstring str2){
 zstring newZString(const char* str){
     zstring result;
     result.data = malloc(lenOfStr(str));
-    size_t  i = 0;
-    while(str[i]  != '\0'){
+
+    if(result.data == NULL){
+        printf("Could not allocate memory for the new ZString. Quitting.\n");
+        exit(1);
+    }
+
+    size_t i = 0;
+    while(!isaTerminator(str[i])){
         result.data[i] = str[i];
         i++;
     }
     result.data[i] = '\0';
+    result.length = lenOfStr(result.data);
     return result;
 }
 
 zstring toLowercaseStr(zstring str){
-    size_t  i = 0; 
-    zstring lstr = newZString("");
-    while(str.data[i] != '\0'){
-        if(str.data[i] >= 'A' && str.data[i] <= 'Z'){
+    size_t i = 0; 
+    zstring lstr = newZString(str.data);
+    while(!isaTerminator(str.data[i])){
+        if(str.data[i] >= 'A' && str.data[i] <= 'Z' && !isaSpace(str.data[i]) && !isaNumber(str.data[i]) && !isaNewLine(str.data[i])){
             lstr.data[i] = str.data[i] + 32;
-        }else {
+        }else{
             lstr.data[i] = str.data[i];
         }
         i++;
     }
     lstr.data[i] = '\0';
+    lstr.length = lenOfStr(lstr.data);
     return lstr;
 }
 
 zstring toUppercaseStr(zstring str){
-    size_t  i = 0; 
-    zstring ustr = newZString("");
-    while(str.data[i] != '\0'){
+    size_t i = 0; 
+    zstring ustr = newZString(str.data);
+    while(!isaTerminator(str.data[i])){
         if(str.data[i] >= 'A' && str.data[i] <= 'Z'){
             ustr.data[i] = str.data[i] + 32;
         }else {
@@ -175,13 +213,14 @@ zstring toUppercaseStr(zstring str){
 }
 
 zstring trimStr(zstring str){
-    zstring result = newZString("");
-    size_t  i = 0; 
-    size_t  j = 0; 
-    while(str.data[i] != '\0'){
-        if(!isaSpace(str.data[i])){
-            result.data[j++] = str.data[i];
+    zstring result = newZString(str.data);
+    size_t i = 0; 
+    size_t j = 0; 
+    while(!isaTerminator(str.data[i])){
+        if(isaSpace(str.data[i])){
+            i++;
         }
+        result.data[j++] = str.data[i];
         i++;
     }
     result.data[j] = '\0';
@@ -190,9 +229,9 @@ zstring trimStr(zstring str){
 }
 
 zstring removeWord(zstring str, char* word){
-    size_t  i = 0; int j = 0;
-    zstring result = newZString("");
-    while(str.data[i] != '\0'){
+    size_t i = 0; int j = 0;
+    zstring result = newZString(str.data);
+    while(!isaTerminator(str.data[i])){
         if(str.data[i] == word[0]){
             if(isWordInString(str, word, i)){
                 i += lenOfStr(word);
@@ -208,10 +247,10 @@ zstring removeWord(zstring str, char* word){
 }
 
 zstring removeChar(zstring str, char c){
-    zstring result = newZString("");
-    size_t  i = 0; 
-    size_t  j = 0; 
-    while(str.data[i] != '\0'){
+    zstring result = newZString(str.data);
+    size_t i = 0; 
+    size_t j = 0; 
+    while(!isaTerminator(str.data[i])){
         if(str.data[i] != c){
             result.data[j++] = str.data[i];
         }
@@ -230,8 +269,8 @@ zstring copyStr(zstring str){
 
 zstring reverseStr(zstring str){
     size_t i = 0;
-    zstring result = newZString("");
-    while(str.data[i] != '\0'){
+    zstring result = newZString(str.data);
+    while(!isaTerminator(str.data[i])){
         result.data[i] = str.data[str.length - i - 1];
         i++;
     }
