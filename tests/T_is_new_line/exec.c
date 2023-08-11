965b8e601b451760b5121c7d300b2bb1181059e3
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-08-11 16:58:52 -0300

    att

---
+++ tests/T_is_new_line/exec.c
@@ -0,0 +1,16 @@
+
+
+
+#include <stdio.h>
+
+#define ZSTRING_IMPLEMENTATION
+#include "../../src/zstring.h"
+
+int main(int argc, char *argv[]){
+
+    
+    printf("%d",isaNewLine('\n'));    
+    
+
+    return 0;
+}
